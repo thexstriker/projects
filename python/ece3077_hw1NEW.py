41334b1da4e5eb4bd74e35abeba9e63b490f33fe
Sean Kim <sean.siwon.kim@gmail.com>
2026-09-14 16:47:35 -0400

    working on homework brosky

---
+++ python/ece3077_hw1NEW.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+rng = np.random.default_rng(3077)
+n_trials = 100000 #CHANGED THIS NUMBER!!!
+die4 = rng.integers(1, 5, size=n_trials)
+die5 = rng.integers(1, 6, size=n_trials)
+product_is_even = (die4 * die5) % 2 == 0
+sum_condition = (
+((die4 + die5) <= 3)
+| ((die4 + die5) > 7)
+)
+event = product_is_even & sum_condition
+estimated_probability = np.mean(event)
+
+print(
+f"Estimated probability: "
+f"{estimated_probability:.4f}"
+)
---
+++ python/ece3077_hw1_12.py
@@ -0,0 +1,27 @@
+
+
+class Link:
+    def __init__(self, capacity, avail):
+        self.capacity = capacity
+        self.avail = avail
+        pass
+
+link1 = Link(10, 0.95)
+link2 = Link(10, 0.90)
+link3 = Link(5, 0.92)
+link4 = Link(5, 0.88)
+link5 = Link(2.5, 0.97)
+link6 = Link(2.5, 0.94)
+
+count = 1
+for i in range(2):
+    for j in range(2):
+        for k in range(2):
+            for l in range(2):
+                for m in range(2):
+                    for n in range(2):
+                        sum = ((0 if i % 2 == 0 else link1.capacity) + (0 if j % 2 == 0 else link2.capacity) + (0 if k % 2 == 0 else link3.capacity) + (0 if l % 2 == 0 else link4.capacity) + (0 if m % 2 == 0 else link5.capacity) + (0 if n % 2 == 0 else link6.capacity))
+                        print(str(count) + " " + (str(i) + str(j) + str(k) + str(l) + str(m) + str(n)) + " " + str(sum) + (" TRUE" if sum>20 else " FALSE"))
+                        count+=1
+
+
